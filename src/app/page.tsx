06a54229f7b311dba1491f33dfe6db5a9634db9a
Pustam Rai <github.pustam@gmail.com>
2025-06-23 19:06:40 +0545

    move feature and hero section from home to components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import HeroSection from "@/components/home/HeroSection";
-import FeatureSection from "@/components/home/FeatureSection";
+import HeroSection from "@/components/HeroSection";
+import FeatureSection from "@/components/FeatureSection";
 
 export default function Homepage() {
   return (
